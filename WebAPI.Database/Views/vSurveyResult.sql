a9d5c3fc21420d07ca2dc92030451e772def93c3
Lazar Ilic <lazar.ilic@softlab.rs>
2022-11-14 15:03:55 +0100

    Edit View SurveyResult

--- WebAPI.Database/Views/vSurveyResult.sql
+++ WebAPI.Database/Views/vSurveyResult.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [SurveyResult].[vSurveyResult]
 AS 
-SELECT P.[Id], P.[Name] AS ProductName, P.[BrandId], B.[Name] AS BrandName,
+SELECT P.[Id] AS ProductId, P.[Name] AS ProductName, P.[BrandId], B.[Name] AS BrandName,
 	   AVG(CAST(SR.[Rating] AS Float)) AS Average
 FROM [SurveyResult].[SurveyResult] AS SR
 JOIN [Product].[Product] AS P ON P.[Id] = SR.[ProductId]
