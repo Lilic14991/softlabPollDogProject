2029fc6295dd239fc6e28ee4edb12da19cf904da
Lazar Ilic <lazar.ilic@softlab.rs>
2022-11-22 15:22:16 +0100

    Setup AutoMapper

---
+++ FruitsPoll/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿// -------------------------------------------------------------------------------
+// <copyright file="SurveyResultProfile.cs" company="SoftLab">
+// Copyright (c) www.SoftLab.rs. All rights reserved.
+// </copyright>
+// -------------------------------------------------------------------------------
+namespace PollDog.API.AutoMapper
+{
+    using global::AutoMapper;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Models = WebAPI.Core.Models;
+
+    /// <summary>SurveyResult mapper profile class.</summary>
+    public class AutoMapperProfile : Profile
+    {
+        /// <summary>Initializes a new instance of the <see cref="AutoMapperProfile" /> class.</summary>
+        public AutoMapperProfile()
+        {
+            this.CreateMap<DTO.SurveyResult, Models.SurveyResult>();
+        }
+    }
+}
