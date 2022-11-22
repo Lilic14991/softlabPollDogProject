92b68d2931aacbf0b72342cfa8de6f25c7839b13
Lazar Ilic <lazar.ilic@softlab.rs>
2022-11-22 15:24:39 +0100

    ProductAverageRating Models, DTO, Models, DbModels

---
+++ FruitsPoll/DTO/ProductAverageRatings.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------
+// <copyright file="GetProductAverageRatingsResponse.cs" company="SoftLab">
+// Copyright (c) www.SoftLab.rs. All rights reserved.
+// </copyright>
+// -------------------------------------------------------------------------------
+namespace PollDog.API.DTO;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>Product with average rating.</summary>
+public class ProductAverageRatings
+{
+    /// <summary>Gets or sets the name of the brand.</summary>
+    /// <value>The name of the brand.</value>
+    public string BrandName { get; set; }
+
+    /// <summary>Gets or sets the name of the product.</summary>
+    /// <value>The name of the product.</value>
+    public string ProductName { get; set; }
+
+    /// <summary>Gets or sets the average.</summary>
+    /// <value>The average.</value>
+    public double Average { get; set; }
+}
---
+++ WebAPI.Core/Models/ProductAverageRatings.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------------------------------
+// <copyright file="ProductAverageRatings.cs" company="SoftLab">
+// Copyright (c) www.SoftLab.rs. All rights reserved.
+// </copyright>
+// -------------------------------------------------------------------------------
+namespace WebAPI.Core.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>ProductAverageRating response class.</summary>
+    public class ProductAverageRatings
+    {
+        /// <summary>Gets or sets the name of the brand.</summary>
+        /// <value>The name of the brand.</value>
+        public string BrandName { get; set; }
+
+        /// <summary>Gets or sets the name of the product.</summary>
+        /// <value>The name of the product.</value>
+        public string ProductName { get; set; }
+
+        /// <summary>Gets or sets the average.</summary>
+        /// <value>The average.</value>
+        public double Average { get; set; }
+    }
+}
