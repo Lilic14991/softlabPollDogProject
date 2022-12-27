98dd43e2d77e18018544aa4f8a9149551a764ae2
Lazar Ilic <lazar.ilic@softlab.rs>
2022-12-27 17:21:15 +0100

    Sql Rating range validation

--- WebAPI.Database/Procedures/SurveyResult.Create.sql
+++ WebAPI.Database/Procedures/SurveyResult.Create.sql
@@ -9,7 +9,7 @@ BEGIN
     BEGIN
         ;THROW 50000, 'PRODUCT_IDENTIFIER_IS_NOT_VALID', 1
     END
-    IF @Rating IS NULL OR @Rating <= 0
+    IF @Rating IS NULL OR (@Rating <= 0 OR @Rating >= 5)
     BEGIN
         ;THROW 50002, 'RATING_CANNOT_BE_NULL', 1
     END
